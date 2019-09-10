9a21f7513608b41b0c776c7aca54329234075e19
Дмитрий Дудин <svetoz@gmail.com>
2019-09-10 09:51:38 +0200

    devise — setup default controllers & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#test'
   
-  devise_for :clients, class_name: 'Client::Client', path: 'client'
-  devise_for :staffs,  class_name: 'Staff::Staff',   path: 'staff'
+  devise_for :clients, class_name: 'Client::Client', module: 'clients'
+  devise_for :staffs,  class_name: 'Staff::Staff',   module: 'staffs'
 end
