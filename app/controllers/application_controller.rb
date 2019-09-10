e92d4feb5d6321d1cdba90947da85613d31b7fca
Дмитрий Дудин <svetoz@gmail.com>
2019-09-10 16:13:07 +0200

    run different Vue application for Client & Staff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if resource.class == Client::Client
-      path = Rails.application.routes.url_helpers.clients_app_show_path(resource)
+      clients_app_show_path(resource)
     elsif resource.class == Staff::Staff
-      path = Rails.application.routes.url_helpers.staffs_app_show_path(resource)
-    end
-    path || root_path
+      staffs_app_show_path(resource)
+    end || root_path
   end
 end
---
+++ app/javascript/components/staff.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Staff!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
